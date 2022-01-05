34acc585c5ebed303df19ffeb8c2259623dfb81b
rubens.curvello <rubens.curvello@hotmail.com>
2022-01-05 10:52:52 -0300

    new query mssql

--- src/repository.ts
+++ src/repository.ts
@@ -3,7 +3,7 @@ import { AbstractRepository } from "sdz-agent-types";
 export default class SQLRepository extends AbstractRepository {
     execute(query: string, page?: number, limit?: number): Promise<any> {
         const statement = [
-          query,
+          this.buildQuery(query),
           "undefined" !== typeof page && limit ? `ORDER BY	(SELECT NULL) OFFSET ${page * limit} ROWS FETCH NEXT ${limit} ROWS ONLY` : null,
         ]
         
@@ -13,7 +13,7 @@ export default class SQLRepository extends AbstractRepository {
       }
 
       async count(entity: string): Promise<number> {
-        const resultSet = await this.execute(`SELECT COUNT (*) AS total FROM (${this.loadFile(entity)}) as tab1`);
+        const resultSet = await this.execute(`SELECT COUNT (*) AS total FROM (${this.buildQuery(this.loadFile(entity))}) as tab1`);
         const obj:any = {}
         Object.keys(resultSet).map((key) =>  obj[key.toLowerCase()] = resultSet[key])
         return obj[0].total;
