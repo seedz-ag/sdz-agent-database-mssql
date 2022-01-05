d8a52e188111410ee20b4af50eb4dd4ada6f447e
rubens.curvello <rubens.curvello@hotmail.com>
2022-01-05 15:17:10 -0300

    build

--- dist/repository.js
+++ dist/repository.js
@@ -4,7 +4,7 @@ const sdz_agent_types_1 = require("sdz-agent-types");
 class SQLRepository extends sdz_agent_types_1.AbstractRepository {
     execute(query, page, limit) {
         const statement = [
-            query,
+            this.buildQuery(query),
             "undefined" !== typeof page && limit ? `ORDER BY	(SELECT NULL) OFFSET ${page * limit} ROWS FETCH NEXT ${limit} ROWS ONLY` : null,
         ]
             .filter((item) => !!item)
@@ -12,7 +12,7 @@ class SQLRepository extends sdz_agent_types_1.AbstractRepository {
         return this.getConnector().execute(statement);
     }
     async count(entity) {
-        const resultSet = await this.execute(`SELECT COUNT (*) AS total FROM (${this.loadFile(entity)}) as tab1`);
+        const resultSet = await this.execute(`SELECT COUNT (*) AS total FROM (${this.buildQuery(this.loadFile(entity))}) as tab1`);
         const obj = {};
         Object.keys(resultSet).map((key) => obj[key.toLowerCase()] = resultSet[key]);
         return obj[0].total;
